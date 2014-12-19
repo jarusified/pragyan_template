b69795906c72516622c43317e88a5ed304ab4a25
jarusified <jarus3001@gmail.com>
2014-12-19 22:42:02 +0530

    some kind of working version

--- space_events/scripts/main.js
+++ space_events/scripts/main.js
@@ -28,15 +28,13 @@
 			$('.image-main').css("display","none");
 			$(this).parent().siblings('.cluster-content').css("display","block");*/
 			var section=$(this).parent().attr('id');
-			console.log(section);
 			var outer  =$('#'+section+'.outer');
 			var inner  =$('#'+section+'.inner');
 			$('.cluster-section').removeClass('highlight');
 			$('.cluster-section').css({"left":"0%","z-index":"1","width":"20%","box-shadow":"none"});
-			$('.inner').css({"opacity":"1","left":"0%","width":"25%","z-index":1});
-			$('.cluster-section > img').css({"width":"40px","height":"40px","position":"absolute"});
+			$('.inner').css({"opacity":"1","left":"0%","width":"4%","z-index":1,"display":"none"});
+			$(outer).children('.left-image').css({"opacity":"1","left":"0"});
 			$('.cluster-section p').css("opacity","0");
-			console.log($(outer));
 			$(outer).children().siblings('#sub-cluster-menu').css({"opacity":"1","left":"0%","width":"25%","z-index":"1"});
 			$('.image-main').css("display","none");
 			$(inner).parent().siblings('.cluster-content').css("display","block");
@@ -55,9 +53,10 @@
 			}
 		});
 		$('.back').bind('click',function(){
-			$('.inner').css("display","block");
-			$('.outer').css({"left":"50%","z-index":1,"opacity":"0","width":"25%"});
+			$('.cluster-section').first().addClass('highlight');
 			$('.cluster-section').css({"left":"0%","z-index":1,"width":"50%","box-shadow":"inset 4px 0 10px #68A1CE, inset -20px 0 30px #122c36, 6px -6px 12px -7px #A0DDFF"});
+			$('.inner').css({"display":"block","width":"50%"});
+			$('.outer').css({"left":"50%","z-index":1,"opacity":"0","width":"25%"});
 			$('.cluster-section p').css("opacity","1");
 
 		});
